121d0a472a5afab57f24f68c2931575015569685
giorgio laera <laeragiorg@gmail.com>
2025-10-26 12:01:03 +0100

    complete

--- main.js
+++ main.js
@@ -4,23 +4,36 @@ const inputEtà = prompt("inserire l'età");
 // creo due variabili per convertire i prompt in num
 const km = parseInt(inputKm);
 const età =parseInt(inputEtà);
-const priceForKm = 0.21
-let costokm= NaN
 console.log(km, età);
+const priceForKm = 0.21;
+const under18=0.2;
+const over65=0.4;
+let costoKm= NaN;
+let sconto= NaN;
+let price =NaN;
 
+//calccolo prezzo per km
 if (km==0){
 
-    alert("inserire km")
+    alert("inserire km");
 }
 else{
-  costokm=km*priceForKm 
-//   alert(costokm)
+  costoKm=km*priceForKm 
 }
-if(età< 18){
-    alert("paghi meno")
-}else if(età >=65){
-       alert("paghi ancora meno")
+//calcolo sconto
+if(età< 18){ //uder 18
+sconto = costoKm*under18;
+price = costoKm-sconto;
+document.getElementById("priceTiket").innerHTML = price.toFixed(2);
+   // alert(price.toFixed(2));
+}else if(età >=65){ //over65
+    sconto = costoKm*over65;
+price = costoKm-sconto;
+document.getElementById("priceTiket").innerHTML = price.toFixed(2);
+
 }else{
-       alert("paghi dippiù")
+    price=costoKm
+       document.getElementById("priceTiket").innerHTML = price.toFixed(2);
+
 }
 
