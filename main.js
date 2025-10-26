c32bb5d0f9d7ca3df6f0249fef3d16215e40f836
giorgio laera <laeragiorg@gmail.com>
2025-10-26 10:51:09 +0100

    creation const

---
+++ main.js
@@ -0,0 +1,9 @@
+//creo due variabili per eta e km da percorere
+const inputKm = prompt("inserire i km da percorrere");
+const inputEtà = prompt("inserire l'età");
+// creo due variabili per convertire i prompt in num
+const km = parseInt(inputKm);
+const età =parseInt(inputEtà);
+
+console.log(km, età);
+
