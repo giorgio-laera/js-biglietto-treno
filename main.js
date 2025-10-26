b595efcdadd35b1e442da147e39a28ba3094f7d1
giorgio laera <laeragiorg@gmail.com>
2025-10-26 11:11:02 +0100

    if else complete

--- main.js
+++ main.js
@@ -4,6 +4,23 @@ const inputEtà = prompt("inserire l'età");
 // creo due variabili per convertire i prompt in num
 const km = parseInt(inputKm);
 const età =parseInt(inputEtà);
-
+const priceForKm = 0.21
+let costokm= NaN
 console.log(km, età);
+
+if (km==0){
+
+    alert("inserire km")
+}
+else{
+  costokm=km*priceForKm 
+//   alert(costokm)
+}
+if(età< 18){
+    alert("paghi meno")
+}else if(età >=65){
+       alert("paghi ancora meno")
+}else{
+       alert("paghi dippiù")
+}
 
